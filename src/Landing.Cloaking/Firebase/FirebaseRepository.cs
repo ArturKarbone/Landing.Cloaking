8d7416001d7fa9b5c6bd438a8889a0ffd3b7b287
Artur Karbone <artur.karbone@gmail.com>
2017-07-20 15:40:03 +0300

    introduce firebase

---
+++ src/Landing.Cloaking/Firebase/FirebaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using CoreFire;
+using System;
+
+namespace Landing.Cloaking.Firebase
+{
+    class FirebaseRepository
+    {
+        public string Url => Environment.GetEnvironmentVariable("firebase-url");
+
+        public Client Client
+        {
+            get
+            {
+                return Client.Builder()
+                    .WithUri(new Uri(Url))
+                    .Build();
+            }
+        }
+    }
+}
