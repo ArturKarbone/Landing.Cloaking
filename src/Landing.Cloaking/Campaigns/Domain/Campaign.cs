2853cc9f4adebefdd4b79053a54926f972d74c94
Artur Karbone <artur.karbone@gmail.com>
2017-07-27 16:39:00 +0300

    display IPs

--- src/Landing.Cloaking/Campaigns/Domain/Campaign.cs
+++ src/Landing.Cloaking/Campaigns/Domain/Campaign.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Landing.Cloacking.Models
 {
     public class Campaign
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        [DisplayName("White Landing Url")]
         public string WhiteLandingUrl { get; set; }
+        [DisplayName("Black Landing Url")]
         public string BlackLandingUrl { get; set; }
+        [DisplayName("Cloaking Url")]
         public string CloackingUrl { get; set; }
         public List<IPEntry> LoggedIPs { get; set; } = new List<IPEntry>();
 
